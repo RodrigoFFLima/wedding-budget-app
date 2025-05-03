743385cb56a51cc2330fff67b402fb7402e6c342
RodrigoFFLima <rodrigofflima@hotmail.com>
2025-05-02 22:09:24 -0400

    feat: including the correct structure of project

---
+++ web/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div className="bg-primary p-6 text-center text-white">
+      <h1 className="text-3xl font-bold">Controle de Gastos do Casamento 💒</h1>
+      <p className="mt-4 text-lg">Bem-vindo ao aplicativo!</p>
+      <button className="bg-primary text-white p-2 rounded-md">
+        Clique aqui
+      </button>
+    </div>
+  );
+};
+
+export default Home;
